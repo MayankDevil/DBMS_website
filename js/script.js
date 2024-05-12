bb78efdc4d8634309db53f67317eafa013919d7e
मयंक <mastermayank91@gmail.com>
2024-05-12 12:36:16 +0000

    Update js/script

--- js/script.js
+++ js/script.js
@@ -10,39 +10,39 @@ try
         -------------------
         [ NAVBAR FUNCTION ] ===========================================
         -------------------
-    */ 
-    let navbtn = document.getElementById('nav-btn');
+    */
+        let main = document.getElementById('main')
+        let menu = document.getElementById('aside')
 
-    let aside = document.getElementById('aside');
-    
-    var active = true; // navbar activation key
-    
-    // nav open function
-    function nav_open()
-    {
-        aside.style.display = "block";
-        navbtn.innerHTML = "&#10096;";
-        active = true;
-    }
-    // nav close function
-    function nav_close()
-    {
-        aside.style.display = "none";
-        navbtn.innerHTML = "&#9776;";
-        active = false;
-    }
-    /*
-        NavBar Button function : onclick if active so close else open
-    */ 
-    navbtn.onclick = () => (active)? nav_close() : nav_open();
+        
+        var active = true;
+        
+        function menu_open(){
+            menu.style.display = "block"
+            main.style.gridTemplateColumns = "3fr 1fr"
+            active = true
+        }
+        function menu_close(){
+            menu.style.display = "none"
+            main.style.gridTemplateColumns = "1fr"
+            active = false
+        }
+        
+        // menu button function
+        document.getElementById('menu_button').onclick = () => (active)? menu_close() : menu_open()
+        
+        // menu anchor function
+        document.querySelectorAll('#aside > a').forEach(i => { i.onclick = () => menu_close() })
+
+
     /*
         ----------------------
         [ SEARCHBAR FUNCTION ] ========================================
         ----------------------
     */
-    let searchbar = document.getElementById('searchbar');
+    let searchbar = document.getElementById('searchbar')
 
-    let asidelinks = document.querySelectorAll('#aside  a');
+    let asidelinks = document.querySelectorAll('#aside  a')
     // search function : on key press loop to match value to anchor
     searchbar.onkeyup = () => {
         
@@ -65,8 +65,11 @@ try
         }
     }
     // [ Anchor Function ]----------------------------------------------
+
     asidelinks.forEach(index => index.onclick = () => nav_close());
+    
     // [ Hide Function  ]-----------------------------------------------------------------
+    
     let lead = document.querySelectorAll('#aside  .lead');
 
     searchbar.onfocus = () => lead.forEach(i => i.style.display = "none");
